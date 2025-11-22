e1a9fc4aef6b266d95538ce2178c17d7cac78bc0
alexander-g <3867427+alexander-g@users.noreply.github.com>
2025-11-22 01:06:05 +0000

    js ci

---
+++ cpp/dep.ts
@@ -0,0 +1,5 @@
+export { 
+    is_array_of_type,
+    is_number_array,
+} from "https://raw.githubusercontent.com/alexander-g/DigIT-Base-UI/7dcbc00a85287c118d95da89d030b9d815976033/frontend/ts/util.ts"
+
